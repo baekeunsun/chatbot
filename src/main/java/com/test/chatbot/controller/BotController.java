2ffbc69b7786dc27764d214ff7ac1c82432c18a3
백은선 <72916415+baekeunsun@users.noreply.github.com>
2023-07-17 16:19:16 +0900

    get

--- src/main/java/com/test/chatbot/controller/BotController.java
+++ src/main/java/com/test/chatbot/controller/BotController.java
@@ -1,9 +1,7 @@
 package com.test.chatbot.controller;
 
 import com.test.chatbot.vo.KeyboardVO;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class BotController {
@@ -14,5 +12,11 @@ public class BotController {
                 (new String[] {"챗봇이랑 대화하기", "공지사항 보기", "FAQ 보기", "1:1 문의하기"});
 
         return keyboard;
+    }
+
+    @GetMapping("/ex/test")
+    @ResponseBody
+    public String getExamplePath() {
+        return "Get some example";
     }
 }
